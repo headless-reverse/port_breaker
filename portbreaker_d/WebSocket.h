e4c1c17a7f1ae68ba966e2ad6fc2d2a8b58e4ba5
headless-reverse <scrcpyshell@gmail.com>
2025-11-24 07:14:38 +0100

    Update WebSocket.h

--- portbreaker_d/WebSocket.h
+++ portbreaker_d/WebSocket.h
@@ -3,31 +3,39 @@
 
 #include <QObject>
 #include <QWebSocketServer>
-#include <QWebSocket>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
+#include <QList>
+#include <QTimer>
+#include <QMutex>
 #include "PortBreaker.h"
 
+class QWebSocket;
 class WebSocket : public QObject {
     Q_OBJECT
 
 public:
-    explicit WebSocket(PortBreaker* breaker, quint16 port, QObject *parent = nullptr);
+    explicit WebSocket(PortBreaker* portBreaker, quint16 port, QObject *parent = nullptr);
     ~WebSocket();
 
 private slots:
     void onNewConnection();
     void processTextMessage(QString message);
     void socketDisconnected();
+    void portBreakerLog(QString msg);
+    void portBreakerWatchdogTriggered(QString vid_pid);
+    void onPeriodicUpdateTimeout();
 
 private:
     QWebSocketServer* m_pWebSocketServer;
     QList<QWebSocket*> m_clients;
     PortBreaker* m_portBreaker;
-    QJsonDocument getDevicesJson();
-    void sendResponse(QWebSocket* client, const QString& type, const QString& message, bool success = true);
-    void broadcast(const QJsonDocument& doc);
+    QTimer* m_periodicUpdateTimer;
+    QMutex m_clientsMutex;
+
+    void processCommand(QWebSocket* pSocket, const QString& command, const QString& data);
+    void sendDeviceList(QWebSocket* pSocket);
+    QJsonObject singleDeviceToJson(const UsbDevice& device); 
+    QString getDeviceStatus(const UsbDevice& device);
+    QString getWakeupStatus(const UsbDevice& device);
 };
 
-#endif // WEBSOCKET_H
+#endif
